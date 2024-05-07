b512c29226d479f370d067e8b18365fd5e05cc8e
OzCodesStuff <100306457+OzCodesStuff@users.noreply.github.com>
2024-05-07 15:32:33 -0500

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -53,19 +53,9 @@ resource "azurerm_virtual_machine" "vm" {
   name                  = "myTFVM"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = azurerm_network_interface.ni.id
+  network_interface_ids = [azurerm_network_interface.nic.id] 
 
-resource "azurerm_network_interface" "nic" {
-  name                = "myNIC"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "myNICConfig"
-    subnet_id                     = azurerm_subnet.mySubnet.id
-    private_ip_address_allocation = "Dynamic"
-  
-vm_size               = "Standard_D2s_v3"
+  vm_size               = "Standard_D2s_v3"
 
   delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
@@ -92,12 +82,64 @@ vm_size               = "Standard_D2s_v3"
   os_profile_linux_config {
     disable_password_authentication = false
   }
-
-  # Remove the duplicate definition of the "network_interface_ids" attribute
-  # network_interface_ids = [azurerm_network_interface.ni.id]
 }
 
+resource "azurerm_network_interface" "nic" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
+  ip_configuration {
+    name                          = "myNICConfig"
+    subnet_id                     = azurerm_subnet.mySubnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+  name                  = "myTFVM"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = azurerm_network_interface.ni.id 
+  
+ {
+resource "azurerm_network_interface" "nic" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "myNICConfig"
+    subnet_id                     = azurerm_subnet.mySubnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+  vm_size               = "Standard_D2s_v3"
+
+  delete_os_disk_on_termination    = true
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "myOsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+  }
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = ""
+    admin_password = ""
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
 # Create a Log Analytics workspace
 resource "azurerm_log_analytics_workspace" "law" {
   name                = "myTFLogAnalyticsWorkspace"
