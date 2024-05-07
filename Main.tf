7381b1e762f8b6ca03974f0cc4ffe15646c342dd
OzCodesStuff <100306457+OzCodesStuff@users.noreply.github.com>
2024-05-07 15:29:57 -0500

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -53,7 +53,7 @@ resource "azurerm_virtual_machine" "vm" {
   name                  = "myTFVM"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.ni.id] 
+  network_interface_ids = azurerm_network_interface.ni.id
 
 resource "azurerm_network_interface" "nic" {
   name                = "myNIC"
